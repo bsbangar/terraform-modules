a6617d8757ba41fb1832990147c2a1059f673b14
Balwinder Singh <itssinghb@gmail.com>
2026-07-08 16:22:09 +0100

    update2

--- parent_module/main.tf
+++ parent_module/main.tf
@@ -1,16 +1,16 @@
-module "rg" {
-  source = "../child_module/azurerm_resource_group"
-  rgs    = var.rgs
-}
+# module "rg" {
+#   source = "../child_module/azurerm_resource_group"
+#   rgs    = var.rgs
+# }
 
-module "virtual_network" {
-  depends_on = [module.rg]
-  source     = "../child_module/azurerm_virtual_network"
-  vnets      = var.vnets
-}
+# module "virtual_network" {
+#   depends_on = [module.rg]
+#   source     = "../child_module/azurerm_virtual_network"
+#   vnets      = var.vnets
+# }
 
-module "subnet" {
-  depends_on = [module.virtual_network]
-  source     = "../child_module/azurerm_subnet"
-  snets      = var.snets
-}
+# module "subnet" {
+#   depends_on = [module.virtual_network]
+#   source     = "../child_module/azurerm_subnet"
+#   snets      = var.snets
+# }
